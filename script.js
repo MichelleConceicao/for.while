3a6c7ab85ed811bb978a03c0af8d4b5a3d700033
MichelleConceição <liliandlentretenimento@gmail.com>
2022-06-15 11:36:58 -0300

    desafio3 finalizado

---
+++ script.js
@@ -0,0 +1,20 @@
+for(let number1 =1; number1 <= 10; number1++){console.log("For Loop", number1);
+}
+
+let number2 = 10; 
+while(number2 >= 1){
+    console.log("While Loop:",  number2);
+    number2--; 
+}
+
+let number3 = 1; 
+while(number3 <= 100){
+    console.log("Loop impares:",  number3);
+    number3+=2
+}
+
+let number4 = 0; 
+while(number4 <= 100){
+    console.log("Loop pares:",  number4);
+    number4+=2
+}
